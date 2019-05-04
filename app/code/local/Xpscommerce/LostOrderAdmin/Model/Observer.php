8800f97d376302248d03ac39688b3b175502030d
Andrew <andrew@masterdef.net>
2019-05-04 10:49:24 +0000

    add a new button to orders called "lost order"

--- app/code/local/Xpscommerce/LostOrderAdmin/Model/Observer.php
+++ app/code/local/Xpscommerce/LostOrderAdmin/Model/Observer.php
@@ -6,28 +6,55 @@
 
 
 /**
+ * LostOrderAdmin Observer
+ *
+ * @category    Xpscommerce
+ * @package     Xpscommerce_LostOrderAdmin
+ * @author      @ridestore
  */
 class Xpscommerce_LostOrderAdmin_Model_Observer {
+    /**
+     * Add LostOrder Button to Order View
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Xpscommerce_LostOrderAdmin_Model_Observer
+     */
     public function addButtonLostOrder(Varien_Event_Observer $observer) {
         $block = Mage::app()->getLayout()->getBlock('sales_order_edit');
         if (!$block) {
             return $this;
         }
         $order = Mage::registry('current_order');
-        $url   = Mage::helper("adminhtml")->getUrl("lostorderadmin/adminhtml_refundorder",
-            array('order_id' => $order->getId())
-        );
-        $message = Mage::helper('lostorderadmin')->__('Is this a lost order and have you sent a replacement?');
 
-        $block->addButton(
-            'button_id',
-            array(
-                'label'   => Mage::helper('lostorderadmin')->__('Lost Order'),
-                'onclick' => "confirmSetLocation('{$message}', '{$url}')",
-                'class'   => 'go'
-            )
-        );
+        if (!$order->hasCreditmemos() && $order->hasShipments()) {
+            $this->_addButtonLostOrderToBlock($block, $order);
+        }
+
         return $this;
+    }
+
+    /**
+     * Put button to block
+     * @param Mage_Adminhtml_Block_Sales_Order_View $block
+     * @param Mage_Sales_Model_Order $order
+     * @return Xpscommerce_LostOrderAdmin_Model_Observer
+     **/
+    private function _addButtonLostOrderToBlock(Mage_Adminhtml_Block_Sales_Order_View $block, Mage_Sales_Model_Order $order) {
+            $url   = Mage::helper("adminhtml")->getUrl("lostorderadmin/adminhtml_refundorder",
+                array('order_id' => $order->getId())
+            );
+            $message = Mage::helper('lostorderadmin')->__('Is this a lost order and have you sent a replacement?');
+
+            $block->addButton(
+                'button_id',
+                array(
+                    'label'   => Mage::helper('lostorderadmin')->__('Lost Order'),
+                    'onclick' => "confirmSetLocation('{$message}', '{$url}')",
+                    'class'   => 'go'
+                )
+            );
+        
+            return $this;
     }
 }
 
--- app/code/local/Xpscommerce/LostOrderAdmin/controllers/Adminhtml/RefundorderController.php
+++ app/code/local/Xpscommerce/LostOrderAdmin/controllers/Adminhtml/RefundorderController.php
@@ -4,24 +4,28 @@ class Xpscommerce_LostOrderAdmin_Adminhtml_RefundorderController
     extends Mage_Adminhtml_Controller_Action
     //Mage_Adminhtml_Sales_Order_CreditmemoController
 {
+
+    /**
+        When proceeding I want the following to be made:
+        All qty of the products on the order should be “refunded”
+
+        This because I want the item status to be set to “refunded” (IMPORTANT that the product is NOT put back in stock)
+        The subtotal should then be added to “Adjustment fee” so that we don’t refund this amount to the customer.
+        The Shipping & Handling fee should be added to “Refund Shipping & Handling” so this is refunded to the customer (if 0 then 0 will be refunded to customer)
+        After this is done I want a automessage to be added as a comment “[MAGENTO USER NAME]: Order is lost and has been locked for handling. To refund the customer make a manual creditmemo on this order�
+        I hope you understand my thinking here but just to clarify 
+        I have made a video where I do the same steps. 
+        And as always just ask if something needs to be explained better.
+
+        Video: 
+        https://cl.ly/9f869eb55e92
+    **/
     public function indexAction() {
         $orderId = $this->getRequest()->getParam('order_id');
         $order = Mage::getModel('sales/order')->load($orderId);
 
         if ($this->_canCreditmemo($order)) {
             $creditmemo = $this->_initCreditmemo($order);
-            /*
-            $service = Mage::getModel('sales/service_order', $order);
-            $data = array('qtys' => array());
-
-            foreach ($order->getItemsCollection() as $item) {
-                $data['qtys'][$item->getId()]   = array(
-                    'qty' => $item->getQtyOrdered(),
-                );
-            }
-
-            $creditmemo = $service->prepareCreditmemo($data);
-            */
 
             $creditmemo->setGrandTotal($order->getShippingAmount());
             $creditmemo->setBaseGrandTotal($order->getShippingAmount());
@@ -30,6 +34,11 @@ class Xpscommerce_LostOrderAdmin_Adminhtml_RefundorderController
             $creditmemo->setAdjustment(-($order->getSubtotal()));
             $creditmemo->setRefundRequested(true);
             $creditmemo->register();
+
+
+            $username = Mage::getSingleton('admin/session')->getUser()->getUsername();
+            $order->addStatusHistoryComment("{$username}: Order is lost and has been locked for handling. To refund the customer make a manual creditmemo on this order");
+            $order->save();
 
             $this->_saveCreditmemo($creditmemo);
         }
