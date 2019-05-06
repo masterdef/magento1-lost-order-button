cce2eec3e54bb569bed6aad234ebc3f145f9e616
Andrew <andrew@masterdef.net>
2019-05-06 16:53:16 +0000

    res

--- bin/mysql.sh
+++
@@ -1,1 +0,0 @@
-mysql -u pdmngr_xps --password='daTee6ja' lostorderadmin_xps
--- bin/update.sh
+++
@@ -1,5 +0,0 @@
-rm -rf var/cache/*
-/bin/systemctl reload php-fpm.service
-
-mysqldump -u pdmngr_xps --password='daTee6ja' lostorderadmin_xps > lostorderadmin_xps-`date +'%Y%m'`.sql
-du -s *.sql
